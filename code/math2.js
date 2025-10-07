e0e4b1e5847821c3b4aec6610256dd872361f151
Ratia5 <129026847+Ratia5@users.noreply.github.com>
2025-10-07 17:51:33 -0500

    math2 and lint on push

---
+++ code/math2.js
@@ -0,0 +1,7 @@
+
+function square(val)
+{
+    return val * val;
+}
+
+module.exports = { square };
---
+++ code/math2.test.js
@@ -0,0 +1,9 @@
+
+// Get functions from math2.js
+const { square } = require('./math2.js');
+
+test('Square function test', () => {
+    expect(square(5)).toBe(25);
+});
+
+unused = 5;
